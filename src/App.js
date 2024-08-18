76f446b93ae425963bcd48b8f271fda8fb06147f
Rakesh <rakeshsabbani1902@gmail.com>
2024-08-18 12:26:29 +0530

    Sign In/Sign Up form && Login/Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
+import Body from "./components/Body";
 
-import './App.css';
 
 function App() {
   return (
-    <div className="text-3xl font-bold text-green-800">
-     Namaste Everyone ,Let's build Netflix GPT
+    <div >
+     <Body/>
     </div>
   );
 }
