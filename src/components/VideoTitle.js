d0db7cd8d3f2161d7325de0d428f54b9716ed3cb
Rakeshsabbani1902 <rakeshsabbani1902@gmail.com>
2024-09-11 11:28:48 +0530

    Update VideoTitle.js

--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 const VideoTitle = ({title,overview}) => {
   return (
-    <div className="w-screen aspect-video pt-[20%] px-6 md:px-24 absolute text-white bg-gradient-to-r from-black">
-       <h1 className=" text-2xl md:text-6xl font-bold">{title}</h1>
-       <p className="hidden md:inline-block py-6 text-lg w-1/4">{overview}</p>
+    <div className="md:pt-[18%] pt-[35%] px-20 md:mt-[0%] mt-[12%]  w-screen aspect-video absolute bg-gradient-to-r from-black text-white">
+       <h1 className=" md:text-4xl text-2xl font-bold">{title}</h1>
+       <p className="md:w-1/2 w-auto p-2 text-sm md:text-lg">{overview}</p>
        <div className=" flex my-3 md:m-0">
-        <button className="bg-white text-black py-1 md:py-4 px-3 md:px-12 text-xl  rounded-lg hover:bg-opacity-80"> <img  className ="w-10" alt="play=logo" src="https://miro.medium.com/v2/da:true/resize:fit:768/1*IDJ4x4E-bOypnEZdA5TGHQ.gif" />Play</button>
-        <button className="hidden md:inline-block mx-2  bg-gray-500 text-white p-4 px-12 text-xl bg-opacity-50 rounded-lg">More Info </button>
+        <button className="md:p-4 p-2 md:px-12 px-4 text-sm md:text-xl mx-3 bg-white font-bold rounded-md  text-black hover:bg-opacity-80"> <img  className ="w-10" alt="play=logo" src="https://miro.medium.com/v2/da:true/resize:fit:768/1*IDJ4x4E-bOypnEZdA5TGHQ.gif" />Play</button>
+        <button className="md:p-4 p-2 md:px-12 px-4 text-sm md:text-xl bg-gray-400 font-bold rounded-md bg-opacity-50">More Info </button>
        </div>
     </div>
   )
